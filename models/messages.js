e93bc66ab97be5c83ee4acb707bfb2d963b2c362
angela <amsuarezp@gmail.com>
2020-09-19 22:14:10 -0500

    models

---
+++ models/messages.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../lib/sequelize");
+
+class Message extends Model {}
+
+Message.init(
+  {
+    message: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ts: {
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+  },
+  { sequelize, modelName: "Message" }
+);
+
+Message.sync();
+
+module.exports = Message;
