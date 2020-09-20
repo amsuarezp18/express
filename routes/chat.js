6ad2d8bb903673ba7c142fa2c324526f87a2d39d
angela <amsuarezp@gmail.com>
2020-09-19 22:24:00 -0500

    devuelve json vacios por que aun no creo los datos

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const Message = require('../models/messages');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/api/messages', function(req, res, next) {
+    Message.findAll().then((result) =>{
+        res.send(result);
+    });
+    
+  });
+  
 
 module.exports = router;
